4d27bae007999580f3cc45d54f2343cff8ab7ce5
Goktug Unver <70326299+Goktugunver@users.noreply.github.com>
2022-07-18 00:11:14 +0300

    Add files via upload

---
+++ Assignments/Assigment4.py
@@ -0,0 +1,11 @@
+from traceback import print_tb
+
+
+students = ["Emre Ozsoylu ","Goktug Unver","Burak Baydındır","Yagmur Gonca Saracoglu","Ramzan Oruc"] 
+
+print(students[0],students[1],students[2],students[3])
+
+print("----------------------")
+
+print(students)
+
